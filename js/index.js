6e0fda30216b2567fc22d76406ec39399a558968
guoshuanglovehtml <1460840442@qq.com>
2017-05-29 12:22:59 +0800

    修改

--- js/index.js
+++ js/index.js
@@ -2,19 +2,18 @@ window.onload = function ()
 {   
 	//1 轮播 无缝
     var banner = document.getElementsByClassName('banner')[0];
-    console.log(banner);
-
+    // console.log(banner);
     var ul = banner.children[2].children[0];
-    console.log(ul);
+    // console.log(ul);
 
     var num = 0 ;
     var timer = null;
 
-    timer = setInterval(autoPlay,1);
+    timer = setInterval(autoPlay,3);
     function  autoPlay(argument) 
     {   
     	num--;
-    	num<=-8000 ? num = 0 : num;
+    	num<=-7200 ? num = 0 : num;
         ul.style.left = num + "px";
     };
 
@@ -24,8 +23,12 @@ window.onload = function ()
     }
     ul.onmouseout = function() 
     {
-        timer = setInterval(autoPlay,1);  
+        timer = setInterval(autoPlay,3);  
         // 开启定时器
     }
+
+    // 2、li背景色
+    var colorArr = ["#00bfff","#00ffff",'#3cb371',''];
+
 
 }
