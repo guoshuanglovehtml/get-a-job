a886259fb899217eca041ec78b9f71b39f1fccfd
guoshuanglovehtml <1460840442@qq.com>
2017-06-05 15:58:07 +0800

    添加

---
+++ js/第一问.js
@@ -0,0 +1,9 @@
+window.onload = function ()
+{   
+    var dianji=document.getElementById('dianji')
+	var xianshi=document.getElementById("xianshi")
+    dianji.onclick=function(){
+        xianshi.style.display="block";
+    }
+
+}
